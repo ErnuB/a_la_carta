584e23276262866b1f300ce746d4824ad221e394
Ernu <ernubutchart@gmail.com>
2023-04-27 17:52:57 -0700

    V1.1

--- js/script.js
+++ js/script.js
@@ -2,26 +2,9 @@ const buttonDer =document.getElementById("scroll-der");
 const buttonIzq = document.getElementById("scroll-izq");
 
 buttonDer.onclick = function () {
-    document.getElementById("cards").scrollLeft += 350;
+    document.getElementById("cards").scrollLeft += 370;
 }
 
 buttonIzq.onclick = function () {
-    document.getElementById("cards").scrollLeft -= 350;
+    document.getElementById("cards").scrollLeft -= 370;
 }
-
-window.onload = function() {
-    const modal = document.getElementById("my-modal");
-    const img = document.getElementById("menu-ejemplo");
-    const modalImg = document.getElementById("img01");
-    
-    img.onclick = function() {
-      modal.style.display = "block";
-      modalImg.src = this.src;
-    }
-  
-    const span = document.getElementById("close");
-  
-    span.onclick = function() {
-      modal.style.display = "none";
-    }
-  }
